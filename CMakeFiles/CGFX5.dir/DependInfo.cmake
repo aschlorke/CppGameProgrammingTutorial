9a7f3933db8df2c841e735ce73d7d6f64eed3df8
Aaron Schlorke <aschlorke@yahoo.com>
2021-01-25 21:21:41 -0500

    Initial Commit

---
+++ CMakeFiles/CGFX5.dir/DependInfo.cmake
@@ -0,0 +1,63 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/platform/generic/cmwc4096.c" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/platform/generic/cmwc4096.c.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/rendering/staticLibs/stb_image.c" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/rendering/staticLibs/stb_image.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "headers"
+  "sources"
+  "src"
+  "lib/glew/include"
+  "lib/SDL2/include"
+  "lib/Assimp/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/core/memory.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/core/memory.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/dataStructures/string.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/dataStructures/string.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/main.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/main.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/math/aabb.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/math/aabb.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/math/color.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/math/color.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/math/matrix.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/math/matrix.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/math/plane.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/math/plane.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/math/quaternion.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/math/quaternion.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/math/sphere.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/math/sphere.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/math/transform.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/math/transform.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/math/vecmath.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/math/vecmath.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/math/vector.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/math/vector.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/platform/generic/genericMemory.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/platform/generic/genericMemory.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/platform/opengl/openGLRenderDevice.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/platform/opengl/openGLRenderDevice.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/platform/sdl/sdlApplication.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/platform/sdl/sdlApplication.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/platform/sdl/sdlWindow.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/platform/sdl/sdlWindow.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/platform/sdl/sdltiming.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/platform/sdl/sdltiming.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/rendering/arrayBitmap.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/rendering/arrayBitmap.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/rendering/ddstexture.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/rendering/ddstexture.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/rendering/indexedModel.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/rendering/indexedModel.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/rendering/modelLoader.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/rendering/modelLoader.cpp.o"
+  "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/src/test.cpp" "/Users/aaronschlorke/Projects/Cpp/Tutorials/3DGameProgrammingTutorial/CGFX5/CMakeFiles/CGFX5.dir/src/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "headers"
+  "sources"
+  "src"
+  "lib/glew/include"
+  "lib/SDL2/include"
+  "lib/Assimp/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
