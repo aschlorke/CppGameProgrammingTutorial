25330fef80633040ac8dd89188ceb88cfb42b50b
Aaron Schlorke <aschlorke@yahoo.com>
2021-02-02 13:06:20 -0500

    finished sort and sweep part 1 (video #21)

--- src/interactionWorld.cpp
+++ src/interactionWorld.cpp
@@ -33,3 +33,50 @@ void InteractionWorld::onRemoveComponent(EntityHandle handle, uint32 id)
         entitiesToRemove.push_back(handle);
     }
 }
+
+void InteractionWorld::processInteractions(float delta)
+{
+    removeEntities();
+
+    int axis = findHighestVarianceAxis();
+
+    // Sort AABBs by min on that axis
+    // Go through the list, test intersections in range
+}
+
+void InteractionWorld::removeEntities()
+{
+    if (entitiesToRemove.size() == 0)
+    {
+        return;
+    }
+
+    for (size_t i = 0; i < entitiesToRemove.size(); i++)
+    {
+        bool didRemove = false;
+        do
+        {
+            didRemove = false;
+            for (size_t j = 0; j < entities.size(); j++)
+            {
+                if (entitiesToRemove[i] == entities[j])
+                {
+                    entities.swap_remove(i);
+                    entitiesToRemove.swap_remove(j);
+                    didRemove = true;
+                    break;
+                }
+            }
+            if (didRemove && entitiesToRemove.size() == 0)
+            {
+                return;
+            }
+        } while (didRemove);
+    }
+
+    entitiesToRemove.clear();
+}
+
+int InteractionWorld::findHighestVarianceAxis()
+{
+}
