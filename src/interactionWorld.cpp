a6c7c9a1d7b1726826521d0467fbecc954a70803
Aaron Schlorke <aschlorke@yahoo.com>
2021-02-07 21:36:39 -0500

    Finished interaction (video #25)

--- src/interactionWorld.cpp
+++ src/interactionWorld.cpp
@@ -93,6 +93,9 @@ void InteractionWorld::processInteractions(float delta)
     removeAndUpdateEntities();
     std::sort(entities.begin(), entities.end(), compareAABB);
 
+    Array<BaseECSComponent *> interactorComponents;
+    Array<BaseECSComponent *> interacteeComponents;
+
     Vector3f centerSum(0.0f);
     Vector3f centerSqSum(0.0f);
     for (size_t i = 0; i < entities.size(); i++)
@@ -114,6 +117,40 @@ void InteractionWorld::processInteractions(float delta)
 
             if (aabb.intersects(otherAABB))
             {
+                size_t interactorIndex = i;
+                size_t interacteeIndex = j;
+                for (size_t dummyIndex = 0; dummyIndex < 2; dummyIndex++)
+                {
+                    for (size_t k = 0; k < entities[interactorIndex].interactors.size(); k++)
+                    {
+                        for (size_t l = 0; l < entities[interacteeIndex].interactees.size(); l++)
+                        {
+                            uint32 index = entities[interactorIndex].interactors[k];
+                            if (index == entities[interacteeIndex].interactees[l])
+                            {
+                                Interaction *interaction = interactions[index];
+                                interactorComponents.resize(Math::max(interactorComponents.size(), interaction->getInteractorComponents().size()));
+                                interacteeComponents.resize(Math::max(interacteeComponents.size(), interaction->getInteracteeComponents().size()));
+
+                                for (size_t m = 0; interaction->getInteractorComponents().size(); m++)
+                                {
+                                    interactorComponents[m] = ecs.getComponentByType(entities[interactorIndex].handle, interaction->getInteractorComponents()[m]);
+                                }
+
+                                for (size_t m = 0; interaction->getInteracteeComponents().size(); m++)
+                                {
+                                    interacteeComponents[m] = ecs.getComponentByType(entities[interacteeIndex].handle, interaction->getInteractorComponents()[m]);
+                                }
+
+                                interaction->interact(delta, &interactorComponents[0], &interacteeComponents[0]);
+                            }
+                        }
+                    }
+                    // check the other possibility if the first entity is the interactee instead of the interactor
+                    size_t tempIndex = interactorIndex;
+                    interactorIndex = interacteeIndex;
+                    interacteeIndex = tempIndex;
+                }
                 // if rules say so, then entities[i] interacts with entities[j]
                 // if rules say so, then entities[j] interacts with entities[i]
             }
